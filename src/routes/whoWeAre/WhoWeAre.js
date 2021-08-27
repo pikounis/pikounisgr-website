30051154a7d096d5801cbca5719df26d52275f9e
pikounis <anastasiospikounis@gmail.com>
2021-08-27 15:42:50 +0300

    edit title on who we are

--- src/routes/whoWeAre/WhoWeAre.js
+++ src/routes/whoWeAre/WhoWeAre.js
@@ -1,9 +1,23 @@
 /* eslint-disable */
 
-import React from 'react';
+import styles from './styles';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import I18n from '../../components/I18n/I18n';
+import Container from '@material-ui/core/Container';
 
-const WhoWeAre = () => (
-    <h1>Who we are</h1>
+const WhoWeAre = ({ classes }) => (
+    <Container>
+        <div className={classes.spacer}/>
+        <Typography
+            variant={'h3'}
+            className={classes.title}
+        >
+            <I18n t="yachtSupplies" />
+        </Typography>
+        <div className={classes.spacer}/>
+    </Container>
+
 );
 
-export default WhoWeAre;
+export default withStyles(styles)(WhoWeAre);
